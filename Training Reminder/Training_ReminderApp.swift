e045752b7dfa44968ca4e06e79005b1b48d083d6
Andy Vu <andyvu2026@u.northwestern.edu>
2023-09-04 11:44:17 -0500

    Initial Commit

---
+++ Training Reminder/Training_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Training_ReminderApp.swift
+//  Training Reminder
+//
+//  Created by Andy Vu on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Training_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Training ReminderUITests/Training_ReminderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Training_ReminderUITestsLaunchTests.swift
+//  Training ReminderUITests
+//
+//  Created by Andy Vu on 9/4/23.
+//
+
+import XCTest
+
+final class Training_ReminderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
